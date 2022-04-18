68bf6de477e0da445d24dc9723b41f82070cb116
Алексей <tsyplov80@mail.ru>
2022-04-18 20:05:39 +0300

    rename

---
+++ tsyplov_ensemble/__init__.py
@@ -0,0 +1,8 @@
+from .bagging import BaggingClassifierCustom
+from .random_forest import RandomForestClassifierCustom
+
+
+__all__ = [
+    "BaggingClassifierCustom",
+    "RandomForestClassifierCustom"
+]
